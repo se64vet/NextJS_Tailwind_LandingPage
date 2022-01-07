668f7f26d158ec8f87ec3fef24fe1e805408513a
Dante Mai <mv.thienphu@gmail.com>
2022-01-07 13:17:44 -0500

    update link to public folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,32 +6,32 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        "homeDesktop": "url('../assets/home/background-home-desktop.jpg')",
-        "homeTablet": "url('../assets/home/background-home-tablet.jpg')",
-        "homeMobile": "url('../assets/home/background-home-mobile.jpg')",
+        "homeDesktop": "url('/assets/home/background-home-desktop.jpg')",
+        "homeTablet": "url('/assets/home/background-home-tablet.jpg')",
+        "homeMobile": "url('/assets/home/background-home-mobile.jpg')",
 
         "desDesktop":
-          "url('../assets/destination/background-destination-desktop.jpg')",
+          "url('/assets/destination/background-destination-desktop.jpg')",
         "desTablet":
-          "url('../assets/destination/background-destination-desktop.jpg')",
+          "url('/assets/destination/background-destination-desktop.jpg')",
         "desMobile":
-          "url('../assets/destination/background-destination-desktop.jpg')",
+          "url('/assets/destination/background-destination-desktop.jpg')",
 
-        "crewDesktop": "url('../assets/crew/background-crew-desktop.jpg')",
-        "crewTablet": "url('../assets/crew/background-crew-desktop.jpg')",
-        "crewMobile": "url('../assets/crew/background-crew-desktop.jpg')",
+        "crewDesktop": "url('/assets/crew/background-crew-desktop.jpg')",
+        "crewTablet": "url('/assets/crew/background-crew-desktop.jpg')",
+        "crewMobile": "url('/assets/crew/background-crew-desktop.jpg')",
 
         "techDesktop":
-          "url('../assets/technology/background-technology-desktop.jpg')",
+          "url('/assets/technology/background-technology-desktop.jpg')",
         "techTablet":
-          "url('../assets/technology/background-technology-desktop.jpg')",
+          "url('/assets/technology/background-technology-desktop.jpg')",
         "techMobile":
-          "url('../assets/technology/background-technology-desktop.jpg')",
+          "url('/assets/technology/background-technology-desktop.jpg')",
 
         "vehicleLs":
-          "url('../assets/technology/image-launch-vehicle-landscape.jpg')",
+          "url('/assets/technology/image-launch-vehicle-landscape.jpg')",
         "vehiclePt":
-          "url('../assets/technology/image-launch-vehicle-portrait.jpg')",
+          "url('/assets/technology/image-launch-vehicle-portrait.jpg')",
       },
       fontFamily: {
         "sans": ['"Barlow Condensed"'],
